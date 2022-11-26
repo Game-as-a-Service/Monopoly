fd069f656b2fc0250223baac781bf162e155fb3d
aa89227 <aa89227@gmail.com>
2022-11-26 20:18:22 +0800

    專案初始化 Issue: #2

---
+++ Server/Repositories/ExampleRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Repositories
+{
+    public class ExampleRepository
+    {
+        
+    }
+}
---
+++ Test/Shared/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace SharedTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
