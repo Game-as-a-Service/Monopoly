b3470e1d891539866f6aeac1afc14f8e1d4fcc3a
aa89227 <aa89227@gmail.com>
2023-02-03 23:35:54 +0800

    tdd: #18 #13 1. 玩家擲骰後移動棋子到需要選擇方向的地方 2. 玩家選擇方向後會繼續前進到定點 3. 玩家選擇方向後會繼續前進到需要選擇方向的地方

--- Test/Shared/Utils.cs
+++ Test/Shared/Utils.cs
@@ -14,7 +14,7 @@ public class Utils
             new Block?[] { new Land("F4"),         null,               null,                   null,               new Land("A4"),            null,               null },
             new Block?[] { new Land("Station4"),   null,               new Land("B5"),        new Land("B6"),    new Land("ParkingLot"),    new Land("C1"),    new Land("C2") },
             new Block?[] { new Land("F3"),         null,               new Land("B4"),        null,               new Land("B1"),            null,               new Land("C3") },
-            new Block?[] { new Land("F2"),         new Land("F1"),    new Land("Prison"),    new Land("B3"),    new Land("B2"),            null,               new Land("Station2") },
+            new Block?[] { new Land("F2"),         new Land("F1"),    new Land("Jail"),    new Land("B3"),    new Land("B2"),            null,               new Land("Station2") },
             new Block?[] { null,                    null,               new Land("E3"),        null,               null,                       null,               new Land("D1") },
             new Block?[] { null,                    null,               new Land("E2"),        new Land("E1"),    new Land("Station3"),      new Land("D3"),    new Land("D2") },
         };
