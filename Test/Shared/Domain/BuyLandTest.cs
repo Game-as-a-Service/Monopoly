a244f233972fb41c5f68b5e7b2b3129e13602c68
Bean90638 <bean32618@gmail.com>
2023-02-19 22:35:48 +0800

    tdd: #8 購買土地 1.修改說明訊息 2.改用Assert.ThrowsException驗證錯誤訊息

--- Test/Shared/Domain/BuyLandTest.cs
+++ Test/Shared/Domain/BuyLandTest.cs
@@ -7,7 +7,17 @@ namespace SharedTests.Domain;
 public class BuyLandTest
 {
     [TestMethod]
-    public void 玩家A在空地上__購買土地__擁有土地()
+    [Description(
+        """
+        Given:  玩家A資產1000元，沒有房地產
+                目前輪到A，A在F4上
+                A1是空地，購買價1000元
+        When:   玩家A購買土地
+        Then:   玩家A持有金額為4000
+                玩家A持有房地產數量為1
+                玩家A持有房地產為F4
+        """)]
+    public void 玩家在空地上可以購買土地()
     {
         // Arrange
         // 玩家A持有的金額 5000
@@ -32,11 +42,20 @@ public class BuyLandTest
     }
 
     [TestMethod]
-    [ExpectedException(typeof(Exception), "金額不足")]
-    public void 玩家A在空地上__購買土地__金額不足()
+    [Description(
+        """
+        Given:  玩家A資產500元,沒有房地產
+                目前輪到A,A在F4上
+                A1是空地,購買價1000元
+        When:   玩家A購買土地
+        Then:   顯示錯誤訊息"金額不足"
+                玩家A持有金額為500
+                玩家A持有房地產數量為0
+        """)]
+    public void 金錢不夠無法購買土地()
     {
         // Arrange
-        // 玩家A持有的金額 5000
+        // 玩家A持有的金額 500
         // 玩家A目前踩在價值為1000的土地F4上
         var map = new Map(Utils.SevenXSevenMap());
         var game = new Game("Test", map);
@@ -46,14 +65,13 @@ public class BuyLandTest
         game.SetPlayerToBlock(playerA, "F4", Direction.Up);
 
         // Act
-        // 玩家A進行購買F4
-        game.BuyLand(playerA, "F4");
+        //顯示錯誤訊息"金額不足"
+        Assert.ThrowsException<Exception>(() => game.BuyLand(playerA, "F4"), "金額不足");
 
         // Assert
-        // 玩家A持有金額為5000
+        // 玩家A持有金額為500
         // 玩家A無持有的房地產
-        Assert.AreEqual(5000, playerA.Money);
+        Assert.AreEqual(500, playerA.Money);
         Assert.AreEqual(0, playerA.LandContractList.Count());
-        Assert.IsFalse(playerA.LandContractList.Any(pa => pa.Land.Id == "F4"));
     }
 }
