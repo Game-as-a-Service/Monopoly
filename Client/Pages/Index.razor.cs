37e134b8618f3194256142b75de372c067179aa6
aa89227 <aa89227@gmail.com>
2023-06-10 15:30:55 +0800

    新增Client專案 - Blazor WebAssembly

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Client.Pages;
+
+public partial class Index
+{
+    [Inject] private IDialogService DialogService { get; set; }
+
+    string state = "Message box hasn't been opened yet";
+    private async void OnButtonClicked()
+    {
+        bool? result = await DialogService.ShowMessageBox(
+            "Warning",
+            "Deleting can not be undone!",
+            yesText: "Delete!", cancelText: "Cancel");
+        state = result == null ? "Canceled" : "Deleted!";
+        StateHasChanged();
+    }
+}
