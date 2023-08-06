2a1d54a891ec69b90d315e1806ac6ce23be30cb4
aa89227 <aa89227@gmail.com>
2023-08-06 13:11:40 +0800

    刪除Blazored.LocalStorage

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,3 @@
-using Blazored.LocalStorage;
 using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -10,6 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
