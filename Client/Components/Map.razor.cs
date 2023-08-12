7908cea705120359ae9d4b39728630e4289c3f18
aa89227 <aa89227@gmail.com>
2023-08-12 09:06:17 +0800

    建立svg地圖
    使用svg作為繪製地圖的主要方式

--- Client/Components/Map.razor.cs
+++ Client/Components/Map.razor.cs
@@ -4,7 +4,11 @@ namespace Client.Components;
 
 public partial class Map : ComponentBase
 {
-    private Block[][] Value { get; set; } = null!;
+    private int width = 0;
+    private int height = 0;
+    private readonly int _blockWidth = 133;
+    private readonly int _blockHeight = 148;
+    private Block[][] Data { get; set; } = null!;
     protected override Task OnInitializedAsync()
     {
         var emptyBlock = new EmptyBlock();
@@ -12,23 +16,27 @@ public partial class Map : ComponentBase
         var road = (string id, RoadType roadType) => new Road(id, roadType);
         var stationLand = (string id, LandType landType) => new StationLand(id, landType);
         var stationRoad = (string id, StationRoadType stationType) => new StationRoad(id, stationType);
-        Value = new Block[][]
+        Data = new Block[][]
         {
-            new Block[] { emptyBlock, emptyBlock, land("A1", LandType.Down), land("A2", LandType.Down), stationLand("S1", LandType.Down), land("A3", LandType.Down), land("A4", LandType.Down), land("A5", LandType.Down), emptyBlock, emptyBlock},
-            new Block[] { emptyBlock, new Start(), road("A1", RoadType.row), road("A2", RoadType.row), stationRoad("S1", StationRoadType.row), road("A3", RoadType.row), road("A4", RoadType.row), road("A5", RoadType.corner_upper_right), emptyBlock, emptyBlock },
-            new Block[] { land("E4", LandType.Right), road("E4", RoadType.col), emptyBlock, emptyBlock, land("B1", LandType.Down), land("B2", LandType.Down), land("B3", LandType.Down), road(null, RoadType.col), land("B4", LandType.Down), land("B5", LandType.Down)},
-            new Block[] { stationLand("S4", LandType.Right), stationRoad("S4", StationRoadType.col), land("E2", LandType.Down), land("E1", LandType.Down), road("B1", RoadType.corner_upper_left), road("B2", RoadType.row), road("B3", RoadType.row), new ParkingLot(), road("B4", RoadType.row), road("B5", RoadType.corner_upper_right) },
+            new Block[] { emptyBlock, emptyBlock, land("A1", LandType.Down), land("A2", LandType.Down), stationLand("S1", LandType.Down), land("A3", LandType.Down), land("A4", LandType.Down), land("A5", LandType.Down), emptyBlock, emptyBlock, emptyBlock },
+            new Block[] { emptyBlock, new Start(), road("A1", RoadType.row), road("A2", RoadType.row), stationRoad("S1", StationRoadType.row), road("A3", RoadType.row), road("A4", RoadType.row), road("A5", RoadType.corner_upper_right), emptyBlock, emptyBlock, emptyBlock },
+            new Block[] { land("E4", LandType.Right), road("E4", RoadType.col), emptyBlock, emptyBlock, land("B1", LandType.Down), land("B2", LandType.Down), land("B3", LandType.Down), road(null, RoadType.col), land("B4", LandType.Down), land("B5", LandType.Down), emptyBlock },
+            new Block[] { stationLand("S4", LandType.Right), stationRoad("S4", StationRoadType.col), land("E2", LandType.Down), land("E1", LandType.Down), road("B1", RoadType.corner_upper_left), road("B2", RoadType.row), road("B3", RoadType.row), new ParkingLot(), road("B4", RoadType.row), road("B5", RoadType.corner_upper_right), emptyBlock },
             new Block[] { land("E3", LandType.Right), road("E3", RoadType.corner_bottom_left), road("E2", RoadType.row), road("E1", RoadType.row), new Prison(), land("C3", LandType.Down), land("S3", LandType.Down), land("C2", LandType.Down), land("C1", LandType.Down), stationRoad("S2", StationRoadType.col), land("S2", LandType.Left) },
-            new Block[] { emptyBlock, emptyBlock , emptyBlock , land("C4", LandType.Right) , road("C4", RoadType.corner_bottom_left) , road("C3", RoadType.row) , stationRoad("S3", StationRoadType.row), road("C2", RoadType.row), road("C1", RoadType.row) , road(null, RoadType.corner_bottom_right) }
+            new Block[] { emptyBlock, emptyBlock , emptyBlock , land("C4", LandType.Right) , road("C4", RoadType.corner_bottom_left) , road("C3", RoadType.row) , stationRoad("S3", StationRoadType.row), road("C2", RoadType.row), road("C1", RoadType.row) , road(null, RoadType.corner_bottom_right), emptyBlock }
         };
+
+        // 從Value自動計算地圖大小 每個Block的大小為 133 * 148，間隔 1
+        width = Data[0].Length * _blockWidth + Data[0].Length - 1;
+        height = Data.Length * _blockHeight + Data.Length - 1;
         return base.OnInitializedAsync();
     }
 
     internal abstract record Block(string Id, string? Image = null, string? ImageStyle = null);
 
-    internal record EmptyBlock() : Block("Empty Block", "empty");
+    internal record EmptyBlock() : Block("Empty Block");
 
-    internal record Road(string Id, RoadType RoadType) : Block(Id, $"road {RoadType}");
+    internal record Road(string Id, RoadType RoadType) : Block(Id, $"road_{RoadType}");
 
     internal enum RoadType
     {
